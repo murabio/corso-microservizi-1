fef75fc7516b50e48fc8e3c0c2ec4ab4ec19f5e9
Fabio Muratorio <f.muratorio@reply.it>
2021-08-05 19:25:19 +0200

    first commit

---
+++ user-service/src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.user.controller;
+
+import com.example.user.VO.ResponseTemplateVO;
+import com.example.user.entity.User;
+import com.example.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("UserController - saveUser " + user.getFirstName());
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("UserController - getUserWithDepartment " + userId);
+        return userService.getUserWithDepartment(userId);
+    }
+
+}
